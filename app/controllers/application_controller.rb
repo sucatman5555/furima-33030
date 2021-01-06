4d068d6f3a052236f2651fbde3df2a369064233f
sucatman5555 <sucatman5555@gmail.com>
2021-01-06 12:37:52 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  # Basic認証の実装01
+  before_action :basic_auth
+  # //Basic認証の実装01
+
+  private
+
+  # Basic認証の実装02
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+  # //Basic認証の実装02
+
 end
