a0d90e5482e77ca05804df46ec69cdb9fdce128c
sucatman5555 <sucatman5555@gmail.com>
2021-01-07 18:21:01 +0900

    4.ユーザー管理機能 #RV07

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
     nickname              { 'たらこtarako1' }
     email                 { 'tarako@gmail.com' }
     password              { '12345a' }
+    # 4.ユーザー管理機能 #RV07
+    password_confirmation { '12345a' }
+    # //4.ユーザー管理機能 #RV07
     last_name_kanji       { 'たらこ佐々木' }
     first_name_kanji      { '太郎' }
     last_name_kana        { 'タラコ' }
