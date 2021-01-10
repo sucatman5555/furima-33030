95e075699c273c5013ef88a2cc01ff25a21d692d
sucatman5555 <sucatman5555@gmail.com>
2021-01-10 12:50:02 +0900

    販売手数料、販売履歴表示の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+//販売手数料や販売利益の計算
+require("../counting")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
